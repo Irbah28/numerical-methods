f4445338fd7d355a35a5add1210fa873c2fe19a3
cryptocodecg <cryptocodecg@gmail.com>
2020-02-25 18:48:15 +0700

    adding secant.py

---
+++ secant.py
@@ -0,0 +1,44 @@
+def factorial(a):
+    result = 1
+    for i in range(1, a+1):
+        result = result * i
+
+    return result
+
+def e(n):
+    e = 0.0
+    for i in range(0, n + 1):
+        e = e + (1/factorial(i))
+    return e
+
+def f(x):
+#    return(e(10)**x -5*x**2)
+#    return (x**3 - 2*(x)**2 - 5)
+    return (x**3 - x - 2)
+
+epsilon = 0.00001
+
+x0 = 1
+x1 = 2
+
+x0 = 3
+x1 = 4
+
+x0 = 2
+x1 = 4
+
+
+x_before = 0
+
+x0 = 2
+x1 = 4
+
+i = 0
+while (abs(x - x_before) > epsilon):
+    x_before = x1
+    x = x - (f(x1)*(x1 - x0))/(f(x1)-f(x0))
+    x0 = x1
+    x1 = x
+
+    print(i, "{0:.6f}".format(x), "{0:.6f}".format(abs(x - x_before)))
+    i = i + 1
